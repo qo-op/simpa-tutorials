4ee24636d68b40c5f87fc8f4fd1a68aef64ef488
Yassuo Toda <yassuo.toda@receita.fazenda.gov.br>
2023-05-04 18:34:53 -0300

    0.13

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,48 +1,48 @@
 import * as React from "react";
-import { Helmet } from "react-helmet";
 import MenuBar from "components/MenuBar";
 import NavigationTree from "components/NavigationTree";
 import ModalLayer from "components/ModalLayer";
 import "./Layout.css";
 
-class Layout extends React.Component<{
-  pageTitle: string;
-  children: React.ReactNode;
-}> {
+class Layout extends React.Component<
+  {
+    pageTitle: string;
+    children: React.ReactNode;
+  },
+  { mounted: boolean }
+> {
   render = () => {
     return (
-      <>
-        <div className="Layout BorderLayout">
-          <div className="PageStartBorderLayout">
-            <div className="LineBorderLayout">
-              <div className="StretchBoxLayout">
-                <MenuBar />
-              </div>
-              <div></div>
-              <div></div>
+      <div className="Layout BorderLayout">
+        <div className="PageStartBorderLayout">
+          <div className="LineBorderLayout">
+            <div className="StretchBoxLayout">
+              <MenuBar />
             </div>
-            <div className="SplitPane">
-              <div className="ScrollPane">
-                <nav>
-                  <NavigationTree />
-                </nav>
-              </div>
-              <div>
-                <div className="SplitPaneDivider"></div>
-              </div>
-              <div className="ScrollPane">
-                <div className="Tutorial CenterLayout">
-                  <div className="CenterLayout">
-                    <h1>{this.props.pageTitle}</h1>
-                  </div>
-                  <div>{this.props.children}</div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="SplitPane">
+            <div className="ScrollPane">
+              <nav>
+                <NavigationTree />
+              </nav>
+            </div>
+            <div>
+              <div className="SplitPaneDivider"></div>
+            </div>
+            <div className="ScrollPane">
+              <div className="Tutorial CenterLayout">
+                <div className="CenterLayout">
+                  <h1>{this.props.pageTitle}</h1>
                 </div>
+                <div>{this.props.children}</div>
               </div>
             </div>
           </div>
-          <ModalLayer />
         </div>
-      </>
+        <ModalLayer />
+      </div>
     );
   };
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,14 +43,7 @@ export const Head: HeadFC = () => (
       rel="stylesheet"
       href="https://fonts.googleapis.com/icon?family=Material+Icons"
     />
-    <link
-      rel="preload"
-      href="https://qo-op.github.io/simpa/simpa.css"
-      as="style"
-      onLoad={(event) => {
-        event.currentTarget.rel = "stylesheet";
-      }}
-    />
+    <link rel="stylesheet" href="https://qo-op.github.io/simpa/simpa.css" />
     <script src="https://qo-op.github.io/simpa/simpa.js"></script>
   </>
 );
