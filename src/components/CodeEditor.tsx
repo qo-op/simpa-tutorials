0e9fb29df296008bcb075675120d7f8f20b920db
Yassuo Toda <yassuo.toda@receita.fazenda.gov.br>
2023-05-22 11:08:21 -0300

    0.115

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,34 +1,72 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
 import "prism-themes/themes/prism-vsc-dark-plus.css";
+import "./CodeEditor.css";
 
 const CodeEditorStyle: React.CSSProperties = {
   height: "100vh",
+  backgroundColor: "rgb(30, 30, 30)",
+};
+
+const CodeEditorTextAreaStyle: React.CSSProperties = {
+  border: "none",
+  backgroundColor: "Transparent",
+  color: "White",
+  padding: "0",
+  resize: "none",
+  outline: "none",
+  height: "auto",
 };
 
 const CodeEditor = () => {
+  const click = (ev: React.MouseEvent) => {
+    const target: HTMLElement = ev.target as HTMLElement;
+    if (target instanceof HTMLTextAreaElement) {
+      return;
+    }
+    const textArea: HTMLElement = target.querySelector(
+      "textarea"
+    ) as HTMLElement;
+    textArea.focus();
+  };
   const change = (ev: React.ChangeEvent) => {
     const textArea: HTMLTextAreaElement =
       ev.currentTarget as HTMLTextAreaElement;
+    textArea.style.color = "Transparent";
+    textArea.style.caretColor = "White";
     const codeEditor = textArea.closest(".CodeEditor") as HTMLElement;
-    const codeEditorViewer = codeEditor.querySelector(
-      ":scope .CodeEditorViewer"
+    const codeEditorHighlighter = codeEditor.querySelector(
+      ".CodeEditorHighlighter"
     ) as HTMLElement;
-    const code: string = textArea.value;
+    let code: string = textArea.value;
+    if (code.endsWith("\n")) {
+      code = code + " ";
+      textArea.value = code;
+      textArea.selectionStart = textArea.selectionEnd - 1;
+      textArea.selectionEnd = textArea.selectionEnd - 1;
+    }
     const highlightedCode = Prism.highlight(code, Prism.languages.html, "html");
-    codeEditorViewer.innerHTML = highlightedCode;
+    codeEditorHighlighter.innerHTML = highlightedCode;
   };
-
   return (
-    <div className="CodeEditor BorderLayout" style={CodeEditorStyle}>
-      <div className="PageStart">
-        <textarea onChange={change}></textarea>
-      </div>
-      <div style={{ color: "White", backgroundColor: "Black" }}>
-        <pre>
-          <div className="CodeEditorViewer"></div>
+    <div
+      className="CodeEditor ScrollPane"
+      data-scrollbar-overlay
+      style={CodeEditorStyle}
+      onClick={click}
+    >
+      <div className="LayeredPane">
+        <pre style={{ margin: "0" }}>
+          <code className="language-html">
+            <div className="CodeEditorHighlighter" style={{}}></div>
+          </code>
         </pre>
+        <textarea
+          style={CodeEditorTextAreaStyle}
+          spellCheck="false"
+          onChange={change}
+        ></textarea>
       </div>
     </div>
   );
