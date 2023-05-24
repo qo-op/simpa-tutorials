649fd692c3dcc8fe6560ceef058329000a02bce5
Yassuo Toda <yassuo.toda@receita.fazenda.gov.br>
2023-05-23 22:04:48 -0300

    0.123

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { vs2015 } from "react-syntax-highlighter/dist/esm/styles/hljs";
+/*
 import Prism from "prismjs";
 import "prism-themes/themes/prism-vsc-dark-plus.css";
+*/
 import "./CodeEditor.css";
 import { useAppDispatch } from "app/hooks";
 import {
@@ -12,15 +14,18 @@ import {
 } from "features/ResultPaneSlice";
 
 const CodeEditor = ({ code, language }: { code: string; language: string }) => {
+  const [innerHTML, setInnerHTML] = useState("");
   const ref = useRef(null);
   const dispatch = useAppDispatch();
   useEffect(() => {
     if (ref.current) {
       const textArea: HTMLTextAreaElement = ref.current as HTMLTextAreaElement;
       const codeEditor = textArea.closest(".CodeEditor") as HTMLElement;
+      /*
       const codeEditorHighlighter = codeEditor.querySelector(
         ".CodeEditorHighlighter"
       ) as HTMLElement;
+      */
       let text: string = code;
       if (text.endsWith("\n")) {
         text = text + " ";
@@ -29,29 +34,36 @@ const CodeEditor = ({ code, language }: { code: string; language: string }) => {
       textArea.selectionStart = 0;
       textArea.selectionEnd = 0;
       textArea.focus();
+      setInnerHTML(text);
       switch (language) {
         case "html":
+          /*
           codeEditorHighlighter.innerHTML = Prism.highlight(
             text,
             Prism.languages.markup,
             "markup"
           );
+          */
           dispatch(setHtmlCode(text));
           break;
         case "css":
+          /*
           codeEditorHighlighter.innerHTML = Prism.highlight(
             text,
             Prism.languages.css,
             "css"
           );
+          */
           dispatch(setCssCode(text));
           break;
         case "js":
+          /*
           codeEditorHighlighter.innerHTML = Prism.highlight(
             text,
             Prism.languages.js,
             "js"
           );
+          */
           dispatch(setJavaScriptCode(text));
           break;
       }
@@ -62,7 +74,10 @@ const CodeEditor = ({ code, language }: { code: string; language: string }) => {
     if (target instanceof HTMLTextAreaElement) {
       return;
     }
-    const textArea: HTMLElement = target.querySelector(
+    const codeEditor: HTMLElement = target.closest(
+      ".CodeEditor"
+    ) as HTMLElement;
+    const textArea: HTMLElement = codeEditor.querySelector(
       "textarea"
     ) as HTMLElement;
     textArea.focus();
@@ -77,9 +92,11 @@ const CodeEditor = ({ code, language }: { code: string; language: string }) => {
     const textArea: HTMLTextAreaElement =
       ev.currentTarget as HTMLTextAreaElement;
     const codeEditor = textArea.closest(".CodeEditor") as HTMLElement;
+    /*
     const codeEditorHighlighter = codeEditor.querySelector(
       ".CodeEditorHighlighter"
     ) as HTMLElement;
+    */
     let text: string = textArea.value;
     if (text.endsWith("\n")) {
       text = text + " ";
@@ -87,68 +104,84 @@ const CodeEditor = ({ code, language }: { code: string; language: string }) => {
       textArea.selectionStart = textArea.selectionEnd - 1;
       textArea.selectionEnd = textArea.selectionEnd - 1;
     }
+    setInnerHTML(text);
     switch (language) {
       case "html":
+        /*
         codeEditorHighlighter.innerHTML = Prism.highlight(
           text,
           Prism.languages.markup,
           "markup"
         );
+        */
         dispatch(setHtmlCode(text));
         break;
       case "css":
+        /*
         codeEditorHighlighter.innerHTML = Prism.highlight(
           text,
           Prism.languages.css,
           "css"
         );
+        */
         dispatch(setCssCode(text));
         break;
       case "js":
+        /*
         codeEditorHighlighter.innerHTML = Prism.highlight(
           text,
           Prism.languages.js,
           "js"
         );
+        */
         dispatch(setJavaScriptCode(text));
         break;
     }
   };
+  /*
   return (
     <div
       className="CodeEditor ScrollPane"
       data-scrollbar-overlay
       onClick={click}
     >
-      {process.env.NODE_ENV === "development" ? (
-        <div className="LayeredPane">
-          <textarea
-            spellCheck="false"
-            onFocus={focus}
-            onChange={change}
-            ref={ref}
-          ></textarea>
-          <pre>
-            <code className="language-markup">
-              <div className="CodeEditorHighlighter"></div>
-            </code>
-          </pre>
-        </div>
-      ) : (
+      <div className="LayeredPane">
+        <textarea
+          spellCheck="false"
+          onFocus={focus}
+          onChange={change}
+          disabled={process.env.NODE_ENV !== "development"}
+          ref={ref}
+        ></textarea>
+        <pre>
+          <code className="language-markup">
+            <div className="CodeEditorHighlighter"></div>
+          </code>
+        </pre>
+      </div>
+    </div>
+  );
+  */
+  return (
+    <div
+      className="CodeEditor ScrollPane"
+      data-scrollbar-overlay
+      onClick={click}
+    >
+      <div className="LayeredPane">
+        <textarea
+          spellCheck="false"
+          onFocus={focus}
+          onChange={change}
+          disabled={process.env.NODE_ENV !== "development"}
+          ref={ref}
+        ></textarea>
         <div className="SyntaxHighlighter">
           <SyntaxHighlighter language="xml" style={vs2015} wrapLongLines>
-            {`...
-<div class="BorderLayout"
-     style="height: 100px;">
-  <div class="PageStart">
-    <span>Hello, World!</span>
-  </div>
-  <button>Click me!</button>
-</div>
-...`}
+            {innerHTML}
           </SyntaxHighlighter>
         </div>
-      )}
+      </div>
     </div>
   );
 };
