44924863db15fcd2e47307fd7be8c6d89c8dcb0f
Yassuo Toda <yassuo.toda@receita.fazenda.gov.br>
2023-06-07 00:35:41 -0300

    0.232

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -100,19 +100,22 @@ const CodeEditor = ({
       id={id || ""}
     >
       <div className="BorderLayout">
-        <textarea
-          spellCheck="false"
-          onFocus={focusGained}
-          onChange={change}
-          value={
-            language === "html"
-              ? codeEditorHtmlCode
-              : language === "css"
-              ? codeEditorCssCode
-              : codeEditorJavaScriptCode
-          }
-          disabled={false}
-        ></textarea>
+        {process.env.NODE_ENV === "development" || hostname === "localhost" ? (
+          <textarea
+            spellCheck="false"
+            onFocus={focusGained}
+            onChange={change}
+            value={
+              language === "html"
+                ? codeEditorHtmlCode
+                : language === "css"
+                ? codeEditorCssCode
+                : codeEditorJavaScriptCode
+            }
+          ></textarea>
+        ) : (
+          <></>
+        )}
         <div className="SyntaxHighlighter">
           <SyntaxHighlighter
             language={
